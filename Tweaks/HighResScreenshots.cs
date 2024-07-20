38924a67586df02cfa686f5f62119acf76be0c52
Simon Latusek <Simon.Latusek@gmail.com>
2024-07-20 16:09:23 +0200

    HighRes Screenshots Fix

--- Tweaks/HighResScreenshots.cs
+++ Tweaks/HighResScreenshots.cs
@@ -16,11 +16,14 @@ using Framework = FFXIVClientStructs.FFXIV.Client.System.Framework.Framework;
 
 namespace SimpleTweaksPlugin.Tweaks;
 
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
 [Changelog("1.9.7.1", "Re-added 'Use ReShade' option")]
+[TweakName("Screenshot Improvements")]
+[TweakDescription("Allows taking higher resolution screenshots, Hiding Dalamud & Game UIs and removing the copyright notice from screenshots.")]
+[TweakReleaseVersion("1.8.2.0")]
+[TweakAutoConfig]
 public unsafe class HighResScreenshots : Tweak {
-    public override string Name => "Screenshot Improvements";
-    public override string Description => "Allows taking higher resolution screenshots, Hiding Dalamud & Game UIs and removing the copyright notice from screenshots.";
-    protected override string Author => "NotNite";
 
     private nint copyrightShaderAddress;
 
@@ -52,8 +55,9 @@ public unsafe class HighResScreenshots : Tweak {
     private HookWrapper<ReShadeKeyTest> reShadeKeyTestHook;
 
     private bool updatingReShadeKeybind = false;
-    
-    protected override DrawConfigDelegate DrawConfigTree => (ref bool hasChanged) => {
+
+    protected void DrawConfig(ref bool changed)
+    {
         ImGui.TextWrapped(
             "This tweak will increase the resolution of screenshots taken in game. It will NOT increase the scale of your HUD/plugin windows.");
         ImGui.TextWrapped("Your HUD will appear smaller while the screenshot is processing.");
@@ -68,16 +72,16 @@ public unsafe class HighResScreenshots : Tweak {
         ImGui.TextWrapped("The game WILL crash if you set the scale too high.");
         ImGui.PopStyleColor();
 
-        hasChanged |= ImGui.Checkbox("Use Fixed Resolution", ref Config.UseCustom);
+        changed |= ImGui.Checkbox("Use Fixed Resolution", ref Config.UseCustom);
 
         if (Config.UseCustom) {
 
-            hasChanged |= ImGui.InputInt("Width", ref Config.CustomWidth);
-            hasChanged |= ImGui.InputInt("Height", ref Config.CustomHeight);
+            changed |= ImGui.InputInt("Width", ref Config.CustomWidth);
+            changed |= ImGui.InputInt("Height", ref Config.CustomHeight);
 
         } else {
             ImGui.SetNextItemWidth(ImGuiHelpers.GlobalScale * 100);
-            hasChanged |= ImGui.InputInt("Scale", ref Config.Scale);
+            changed |= ImGui.InputInt("Scale", ref Config.Scale);
 
             ImGui.SameLine();
             var device = Device.Instance();
@@ -85,12 +89,12 @@ public unsafe class HighResScreenshots : Tweak {
         }
         
         ImGui.SetNextItemWidth(ImGuiHelpers.GlobalScale * 100);
-        hasChanged |= ImGui.InputFloat("Delay", ref Config.Delay);
+        changed |= ImGui.InputFloat("Delay", ref Config.Delay);
 
         if (Config.Scale < 1) Config.Scale = 1;
         if (Config.Delay < 0) Config.Delay = 0;
-        hasChanged |= ImGui.Checkbox("Hide Dalamud UI in screenshots", ref Config.HideDalamudUi);
-        hasChanged |= ImGui.Checkbox("Hide Game UI in screenshots", ref Config.HideGameUi);
+        changed |= ImGui.Checkbox("Hide Dalamud UI in screenshots", ref Config.HideDalamudUi);
+        changed |= ImGui.Checkbox("Hide Game UI in screenshots", ref Config.HideGameUi);
         if (copyrightShaderAddress == 0) {
             var disableColor = ImGui.GetColorU32(ImGuiCol.TextDisabled);
             ImGui.PushStyleColor(ImGuiCol.Text, disableColor);
@@ -105,11 +109,11 @@ public unsafe class HighResScreenshots : Tweak {
                 ImGui.SetTooltip("Failed to locate address needed for this option.");
             }
         } else {
-            hasChanged |= ImGui.Checkbox("Remove copyright text", ref Config.RemoveCopyright);
+            changed |= ImGui.Checkbox("Remove copyright text", ref Config.RemoveCopyright);
         }
         
         if (ImGui.Checkbox("Use ReShade to take screenshot", ref Config.UseReShade)) {
-            hasChanged = true;
+            changed = true;
         }
         
         if (Config.UseReShade) {
@@ -145,9 +149,11 @@ public unsafe class HighResScreenshots : Tweak {
             ImGui.Unindent();
             ImGui.Unindent();
         }
-    };
 
-    public override void Setup() {
+        VirtualKey key = Service.KeyState.GetValidVirtualKeys().FirstOrDefault(k => k is not (VirtualKey.CONTROL or VirtualKey.SHIFT or VirtualKey.MENU) && Service.KeyState[k], VirtualKey.NO_KEY);
+    }
+
+    protected override void Setup() {
         AddChangelogNewTweak("1.8.2.0");
         AddChangelog("1.8.3.0", "Added option to hide dalamud UI for screenshot.");
         AddChangelog("1.8.5.0", "Added option to hide game UI for screenshots.");
@@ -164,8 +170,7 @@ public unsafe class HighResScreenshots : Tweak {
             copyrightShaderAddress = 0;
         }
         
-        isInputIDClickedHook ??=
-            Common.Hook<IsInputIDClickedDelegate>("E9 ?? ?? ?? ?? 83 7F 44 02", IsInputIDClickedDetour);
+        isInputIDClickedHook ??= Common.Hook<IsInputIDClickedDelegate>("E9 ?? ?? ?? ?? 83 7F 44 02", IsInputIDClickedDetour);
         isInputIDClickedHook?.Enable();
 
         base.Enable();
@@ -176,13 +181,15 @@ public unsafe class HighResScreenshots : Tweak {
     private uint oldHeight;
     private bool isRunning;
 
-    const int ScreenshotButton = 543;
+    const int ScreenshotButton = 546;
     public bool originalUiVisibility;
     byte[] originalCopyrightBytes = null;
+
     // IsInputIDClicked is called from Client::UI::UIInputModule.CheckScreenshotState, which is polled
     // We change the res when the button is pressed and tell it to take a screenshot the next time it is polled
     private byte IsInputIDClickedDetour(nint a1, int a2) {
-        if (a2 == ScreenshotButton && Config.UseReShade && Framework.Instance()->WindowInactive) return 0;
+        if (a2 == ScreenshotButton && Config.UseReShade && Framework.Instance()->WindowInactive) 
+            return 0;
         
         var orig = isInputIDClickedHook.Original(a1, a2);
         if (AgentModule.Instance()->GetAgentByInternalId(AgentId.Configkey)->IsAgentActive()) return orig;
@@ -212,8 +219,8 @@ public unsafe class HighResScreenshots : Tweak {
             
 
             if (Config.HideGameUi) {
-                var raptureAtkModule = Framework.Instance()->GetUiModule()->GetRaptureAtkModule();
-                originalUiVisibility = !raptureAtkModule->RaptureAtkUnitManager.Flags.HasFlag(RaptureAtkModuleFlags.UiHidden);
+                var raptureAtkModule = Framework.Instance()->UIModule->GetRaptureAtkModule();
+                originalUiVisibility = !raptureAtkModule->RaptureAtkUnitManager.Flags.HasFlag(AtkUnitManagerFlags.UiHidden);
                 if (originalUiVisibility) {
                     raptureAtkModule->SetUiVisibility(false);
                 }
@@ -238,8 +245,8 @@ public unsafe class HighResScreenshots : Tweak {
             Service.Framework.RunOnTick(() => {
                 UIDebug.FreeExclusiveDraw();
                 if (Config.HideGameUi) {
-                    var raptureAtkModule = Framework.Instance()->GetUiModule()->GetRaptureAtkModule();
-                    if (originalUiVisibility && raptureAtkModule->RaptureAtkUnitManager.Flags.HasFlag(RaptureAtkModuleFlags.UiHidden)) {
+                    var raptureAtkModule = Framework.Instance()->UIModule->GetRaptureAtkModule();
+                    if (originalUiVisibility && raptureAtkModule->RaptureAtkUnitManager.Flags.HasFlag(AtkUnitManagerFlags.UiHidden)) {
                         raptureAtkModule->SetUiVisibility(true);
                     }
                 }
